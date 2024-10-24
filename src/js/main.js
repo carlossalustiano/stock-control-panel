c73f10c6ed65421a0218c9e3b8492d04fc04bc90
Carlos <carloseduardo17488@gmail.com>
2024-10-24 14:53:39 -0300

    Identação no código-fonte

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,68 @@
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+  baseURL: "http://localhost:3000",
 });
 
 let produtos;
-let containerEstoque = document.querySelector('#container-stock');
+let containerEstoque = document.querySelector("#container-stock");
 
 async function getProdutos() {
-    try {
-        const response = await api.get('/produtos');
-        produtos = response.data;
-        totalProdutos(produtos);
-        totalSaldo(produtos);
-        mostrarProdutos(produtos);
-        filtrarProdutos(produtos);
-    } catch (error) {
-        console.log('Error no servidor, tente novamente.')
-    }
+  try {
+    const response = await api.get("/produtos");
+    produtos = response.data;
+    totalProdutos(produtos);
+    totalSaldo(produtos);
+    mostrarProdutos(produtos);
+    filtrarProdutos(produtos);
+  } catch (error) {
+    console.log("Error no servidor, tente novamente.");
+  }
 }
 
 function totalProdutos(produtos) {
-    let soma = 0;
-    for(let item in produtos) {
-        soma += produtos[item].quantity;
-    }
+  let soma = 0;
+  for (let item in produtos) {
+    soma += produtos[item].quantity;
+  }
 
-    let h2Quantidade = document.querySelector('.h2Quantidade');
-    h2Quantidade.innerHTML = soma
+  let h2Quantidade = document.querySelector(".h2Quantidade");
+  h2Quantidade.innerHTML = soma;
 }
 
 function totalSaldo(produtos) {
   let somaSaldo = 0;
-  produtos.forEach(produto => {
-    somaSaldo += (produto.quantity * produto.value);
+  produtos.forEach((produto) => {
+    somaSaldo += produto.quantity * produto.value;
   });
-  let totalSaldo = document.querySelector('#text-blue');
+  let totalSaldo = document.querySelector("#text-blue");
   totalSaldo.innerHTML = `R$ ${somaSaldo.toFixed(2)}`;
 }
 
-
 function criarCaixaProduto(produto) {
-  let article = document.createElement('article');
-  article.classList.add('box-stock');
+  let article = document.createElement("article");
+  article.classList.add("box-stock");
 
-  let divNome = document.createElement('div');
-  divNome.classList.add('stock-data');
-  divNome.innerHTML = `<h1>Nome</h1><p>${produto.name}</p>`
+  let divNome = document.createElement("div");
+  divNome.classList.add("stock-data");
+  divNome.innerHTML = `<h1>Nome</h1><p>${produto.name}</p>`;
 
-  let divQuantidade = document.createElement('div');
-  divQuantidade.classList.add('stock-data');
-  divQuantidade.innerHTML = `<h1>Quantidade</h1><p>${produto.quantity}</p>`
+  let divQuantidade = document.createElement("div");
+  divQuantidade.classList.add("stock-data");
+  divQuantidade.innerHTML = `<h1>Quantidade</h1><p>${produto.quantity}</p>`;
 
-  let divValor = document.createElement('div');
-  divValor.classList.add('stock-data');
-  divValor.innerHTML = `<h1>Valor Unitário</h1><p>R$ ${produto.value.toFixed(2)}</p>`
+  let divValor = document.createElement("div");
+  divValor.classList.add("stock-data");
+  divValor.innerHTML = `<h1>Valor Unitário</h1><p>R$ ${produto.value.toFixed(
+    2
+  )}</p>`;
 
-  let divValorTotal = document.createElement('div');
-  divValorTotal.classList.add('stock-data');
-  divValorTotal.innerHTML = `<h1>Valor Total</h1><p>R$ ${(produto.value * produto.quantity).toFixed(2)}</p>`
+  let divValorTotal = document.createElement("div");
+  divValorTotal.classList.add("stock-data");
+  divValorTotal.innerHTML = `<h1>Valor Total</h1><p>R$ ${(
+    produto.value * produto.quantity
+  ).toFixed(2)}</p>`;
 
-  let divBotoes = document.createElement('div');
-  divBotoes.classList.add('buttons');
+  let divBotoes = document.createElement("div");
+  divBotoes.classList.add("buttons");
   divBotoes.innerHTML = `
     <span class="material-icons button-edit">edit_note</span>
     <span class="material-icons button-remove">delete</span>
@@ -75,27 +78,31 @@ function criarCaixaProduto(produto) {
 }
 
 function criarElemento(produto) {
-  let article = document.createElement('article');
-  article.classList.add('box-stock');
+  let article = document.createElement("article");
+  article.classList.add("box-stock");
 
-  let divNome = document.createElement('div');
-  divNome.classList.add('stock-data');
-  divNome.innerHTML = `<h1>Nome</h1><p>${produto.name}</p>`
+  let divNome = document.createElement("div");
+  divNome.classList.add("stock-data");
+  divNome.innerHTML = `<h1>Nome</h1><p>${produto.name}</p>`;
 
-  let divQuantidade = document.createElement('div');
-  divQuantidade.classList.add('stock-data');
-  divQuantidade.innerHTML = `<h1>Quantidade</h1><p>${produto.quantity}</p>`
+  let divQuantidade = document.createElement("div");
+  divQuantidade.classList.add("stock-data");
+  divQuantidade.innerHTML = `<h1>Quantidade</h1><p>${produto.quantity}</p>`;
 
-  let divValor = document.createElement('div');
-  divValor.classList.add('stock-data');
-  divValor.innerHTML = `<h1>Valor Unitário</h1><p>R$ ${produto.value.toFixed(2)}</p>`
+  let divValor = document.createElement("div");
+  divValor.classList.add("stock-data");
+  divValor.innerHTML = `<h1>Valor Unitário</h1><p>R$ ${produto.value.toFixed(
+    2
+  )}</p>`;
 
-  let divValorTotal = document.createElement('div');
-  divValorTotal.classList.add('stock-data');
-  divValorTotal.innerHTML = `<h1>Valor Total</h1><p>R$ ${(produto.value * produto.quantity).toFixed(2)}</p>`
+  let divValorTotal = document.createElement("div");
+  divValorTotal.classList.add("stock-data");
+  divValorTotal.innerHTML = `<h1>Valor Total</h1><p>R$ ${(
+    produto.value * produto.quantity
+  ).toFixed(2)}</p>`;
 
-  let divBotoes = document.createElement('div');
-  divBotoes.classList.add('buttons');
+  let divBotoes = document.createElement("div");
+  divBotoes.classList.add("buttons");
   divBotoes.innerHTML = `
     <span class="material-icons button-edit">edit_note</span>
     <span class="material-icons button-remove">delete</span>
@@ -111,36 +118,35 @@ function criarElemento(produto) {
 }
 
 function mostrarProdutos(produtos) {
-  produtos.forEach(produto => criarCaixaProduto(produto));
+  produtos.forEach((produto) => criarCaixaProduto(produto));
 }
 
 function filtrarProdutos(produtos) {
-  let buttonVoltar = document.querySelector('#button-voltar');
-  let input = document.querySelector('#search');
+  let buttonVoltar = document.querySelector("#button-voltar");
+  let input = document.querySelector("#search");
   let elemento;
 
-  input.addEventListener('keydown', (event) => {
-    if(event.key === 'Enter') {
-
-      buttonVoltar.style.display = 'block';
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      buttonVoltar.style.display = "block";
       let valorBuscado = input.value.toLowerCase();
-      containerEstoque.innerHTML = '';
-  
-      produtos.forEach(produto => {
-        if(valorBuscado === produto.name.toLowerCase()) {
+      containerEstoque.innerHTML = "";
+
+      produtos.forEach((produto) => {
+        if (valorBuscado === produto.name.toLowerCase()) {
           elemento = criarElemento(produto);
           containerEstoque.appendChild(elemento);
         }
       });
-      input.value = '';
+      input.value = "";
     }
-  })
+  });
 
-  buttonVoltar.addEventListener('click', (event) => {
-    buttonVoltar.style.display = 'none';
+  buttonVoltar.addEventListener("click", (event) => {
+    buttonVoltar.style.display = "none";
     containerEstoque.removeChild(elemento);
     mostrarProdutos(produtos);
-  })
+  });
 }
 
-getProdutos()
+getProdutos();
